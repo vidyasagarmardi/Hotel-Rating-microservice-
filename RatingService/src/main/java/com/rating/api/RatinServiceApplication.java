8fa9447710fe2fe5a911863837bf23f61c8d9e5d
vidyasagar <vidyasagarmardi@gmail.com>
2025-01-15 15:08:51 +0530

    Added client discovery dependencies.

--- RatingService/src/main/java/com/rating/api/RatinServiceApplication.java
+++ RatingService/src/main/java/com/rating/api/RatinServiceApplication.java
@@ -2,8 +2,10 @@ package com.rating.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatinServiceApplication {
 
 	public static void main(String[] args) {
